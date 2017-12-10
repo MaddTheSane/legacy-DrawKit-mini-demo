a95bb4f130215adfb3cc3639b2f100c3c9c5bace
C.W. Betts <computers57@hotmail.com>
2017-12-10 14:33:47 -0700

    More Swift conversion.

---
+++ DK Mini Swift Demo/BlankFile.h
@@ -0,0 +1,18 @@
+//
+//  BlankFile.h
+//  DK Mini Swift Demo
+//
+//  Created by C.W. Betts on 12/10/17.
+//
+
+#ifndef BlankFile_h
+#define BlankFile_h
+
+#import <DKDrawKit/DKDrawingView.h>
+
+@interface DKDrawingView (forwardInvocation)
+- (IBAction)selectDrawingToolByName:(id)sender;
+
+@end
+
+#endif /* BlankFile_h */
---
+++ DK Mini Swift Demo/BlankFile.m
@@ -0,0 +1,8 @@
+//
+//  BlankFile.c
+//  DK Mini Swift Demo
+//
+//  Created by C.W. Betts on 12/10/17.
+//
+
+#include "BlankFile.h"
